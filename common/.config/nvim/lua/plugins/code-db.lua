4a02522419db280a201c06f703c3f7b7d1e08145
Pratik Tripathy <mail@pratik.live>
2024-03-17 17:39:13 +0530

    NeoVIM - Setup DB query LSP + autocompletion + treesitter - Setup local AI that works with ollama

---
+++ common/.config/nvim/lua/plugins/code-db.lua
@@ -0,0 +1,42 @@
+local function db_completion()
+    require("cmp").setup.buffer({ sources = { { name = "vim-dadbod-completion" } } })
+end
+
+return {
+    {
+        "tpope/vim-dadbod",
+        opt = true,
+        dependencies = {
+            { "kristijanhusak/vim-dadbod-ui" },
+            { "kristijanhusak/vim-dadbod-completion" },
+        },
+        config = function()
+            vim.g.db_ui_save_location = vim.fn.stdpath("config") .. require("plenary.path").path.sep .. "db_ui"
+            vim.g.db_ui_use_nerd_fonts = 1
+
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = {
+                    "sql",
+                },
+                command = [[setlocal omnifunc=vim_dadbod_completion#omni]],
+            })
+
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = {
+                    "sql",
+                    "mysql",
+                    "plsql",
+                },
+                callback = function()
+                    vim.schedule(db_completion)
+                end,
+            })
+        end,
+        keys = {
+            { "<leader>qq", desc = "Query UI" },
+            { "<leader>qq", "<cmd>DBUIToggle<cr>", desc = "Query UI Toggle" },
+            { "<leader>qa", "<cmd>DBUIAddConnection<cr>", desc = "Query: Add Connection" },
+            { "<leader>qf", "<cmd>DBUIFindBuffer<cr>", desc = "Query: Find Connection" },
+        },
+    },
+}

391f3757ba220d716248dd4cf9e357b27e950a86
Pratik Tripathy <mail@pratik.live>
2024-09-09 23:32:16 +0530

    fix(install): Uncomment brew install chore(install): Rename install test-file to run-test.sh - Better comments on install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,33 +1,34 @@
 #!/usr/bin/env sh
 
 # TODO:
-#    - Display log or show instruction to see the logs
-#    - Add logs to a file
-#    - How to manage/recover from installation issues
+#   - brave, vs-code, cursor, kitty, libreoffice, mattermost-desktop, skypedesktop, ulauncher, code, dotnet8, dotnet8-sdk8.0, aspnetcore-runtime-8.0, sublime-text, AppImage Launcher Settings, Jetbrains Toolbox, Obsidian, Postman, firefox, zen, zoho, zoho-workdrive
+#   - `tee` logs to a file
+#   - README.md
+#   - Integrate with `bootstrap.sh`
 
-# TODO: ppa: brave, vs-code, cursor, kitty, libreoffice, mattermost-desktop, skypedesktop, ulauncher, code, dotnet8, dotnet8-sdk8.0, aspnetcore-runtime-8.0, sublime-text, AppImage Launcher Settings, Jetbrains Toolbox, Obsidian, Postman, firefox, zen, zoho, zoho-workdrive
-
+# Install packages listed on "os-package-list" file
 install_os_packages() {
-    # Install packages listed on "os-package-list" file
     # Ignore lines that start with #
     OS_PACKAGES=$(sed "/^#/d" os-package-list | tr "\n\r" " ")
     sudo apt-get update -y && sudo apt-get upgrade -y && sudo apt-get install $OS_PACKAGES -y
 }
 
+# Install packages listed on "brew-package-list" file
 install_brew_packages() {
-    # Install packages listed on "brew-package-list" file
-    # Ignore lines that start with #
     yes | bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> "$HOME"/.bashrc
     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-    brew tap homebrew/linux-fonts # Required for nerd fonts
+
+    # Required for installing fonts
+    brew tap homebrew/linux-fonts
+
+    # Ignore lines that start with #
     BREW_PACKAGES=$(sed "/^#/d" brew-package-list | tr "\n\r" " ")
     brew install $BREW_PACKAGES
 }
 
 main() {
     install_os_packages
-#    install_brew_packages
+    install_brew_packages
 }
 
 main "$@"
--- scripts/run-test.sh
+++ scripts/run-test.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-# script to build and run docker
-
+# Prune, build and run docker
 main() {
   docker container prune -f && echo "Removed old Docker containers"
   docker build -f ../Dockerfile -t dotfile-install-test:latest .. && echo "Docker build success"

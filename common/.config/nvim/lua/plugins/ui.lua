f3814850ba807a35361b4f539f86a61530f54d14
Pratik Tripathy <mail@pratik.live>
2025-10-13 19:30:27 +0530

    fix(neovim): Unmap `]c`, `[c` & show modified directories in file tree
    - `]c` & `[c` are mapped by default to diffview change traversal

--- common/.config/nvim/lua/plugins/ui.lua
+++ common/.config/nvim/lua/plugins/ui.lua
@@ -44,7 +44,6 @@ return {
                         filetype = "neo-tree",
                         text = "Neo-tree",
                         highlight = "Directory",
-                        text_align = "left",
                     },
                 },
             },

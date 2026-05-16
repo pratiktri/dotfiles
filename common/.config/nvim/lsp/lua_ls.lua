2c36a04a3b0c080fc3315cd82dce4689b71c4826
Pratik Tripathy <mail@pratik.live>
2026-05-16 23:42:45 +0530

    chore(luals): Set workspace boundaries to stop scanning
    - Reduce noisy lsp messages
    - Move `.luarc.json` inside `nvim` - where lua files exist

--- common/.config/nvim/lsp/lua_ls.lua
+++ common/.config/nvim/lsp/lua_ls.lua
@@ -1,11 +1,13 @@
 return {
     cmd = { "lua-language-server" },
     filetype = { "lua" },
+    root_markers = { ".luarc.json", ".luarc.jsonc" },
     settings = {
         Lua = {
             runtime = {
                 version = "LuaJIT",
                 path = vim.split(package.path, ";"),
+                pathStrict = true,
             },
             diagnostics = {
                 globals = { "vim" },

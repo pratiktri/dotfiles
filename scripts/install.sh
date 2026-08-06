5c45160f44363ae64c823fe7d00b43847a4bcc04
Pratik Tripathy <mail@pratik.live>
2026-08-06 19:05:44 +0530

    fix(install): Install `rust-analyzer` post `rustup` installation

--- scripts/install.sh
+++ scripts/install.sh
@@ -31,7 +31,9 @@ rustlang() {
         return
     fi
 
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+    if curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y; then
+        rustup component add rust-analyzer
+    fi
 }
 
 nerd_fonts() {
